091222b6fbbc0756c8a4044f5838373fda8646d7
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2024-09-13 18:48:58 -0400

    Disable default automation

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 /etc/init.d/pwrstatd start
+
+# disable automations
+pwrstat -lowbatt -capacity 0 -shutdown off -active off -runtime 0
+pwrstat -pwrfail -active off -shutdown off
+
 /app/pwrstat-exporter
